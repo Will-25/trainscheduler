a2b54b559c35f80e06d880c02d841b60084e3499
Will <Will.irvin18@gmail.com>
2019-08-30 18:01:57 -0400

    made the JS take in HTML inputs

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+
+$("#submit").on("click", function(){
+    var firstcol = $("#trainName").val().trim();
+    var secondcol = $("#destination").val().trim();
+    var thirdcol = $("#frequency").val().trim();
+    console.log(firstcol);
+    console.log(secondcol);
+    console.log(thirdcol);
+})
