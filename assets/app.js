b65e4b6c6d56918de70e0e8419ec4a84d61f465e
Will <Will.irvin18@gmail.com>
2019-08-30 18:30:17 -0400

    wired up submit button to take inputs

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,26 @@
 
 $("#submit").on("click", function(){
+    
     var firstcol = $("#trainName").val().trim();
     var secondcol = $("#destination").val().trim();
     var thirdcol = $("#frequency").val().trim();
-    console.log(firstcol);
-    console.log(secondcol);
-    console.log(thirdcol);
-})
+    var trainTime = $("#trainTime").val().trim();
+    
+if (firstcol === "" || secondcol === "" || thirdcol === "" || trainTime === "") {
+
+} else {
+
+    var name = $("<div>").text(firstcol)
+    $("#first").append(name);
+
+    var destination = $("<div>").text(secondcol)
+    $("#second").append(destination);
+
+    var frequency = $("<div>").text(thirdcol)
+    $("#third").append(frequency);
+   
+    $("#trainName").val("");
+    $("#destination").val("");
+    $("#frequency").val("");
+    $("#trainTime").val("");
+}});
